f96748e4f08926a819fa77aafc2c2ab5fe41fc2b
Aleksander Fjellvang <Aleksander@fjellvang.dk>
2016-04-09 20:09:19 +0200

    Fish Spawning

---
+++ Assets/Scripts/MoveFish.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveFish : MonoBehaviour {
+
+    public float moveSpeed;
+
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+	}
+
+
+}
