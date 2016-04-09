a3c4eb08b660cc135b63c621bf0d50a14d5c5326
twodordan <twodordan@gmail.com>
2016-04-10 00:50:08 +0200

    Game stuff in my scene

--- Assets/CausticScroll.cs
+++ Assets/CausticScroll.cs
@@ -5,17 +5,34 @@ public class CausticScroll : MonoBehaviour {
 
     public float rotateSpeed = 0.5F;
     public float translateSpeed = 0.5F;
-    public AnimationCurve moveX;
-    public AnimationCurve moveY;
+    //public float translateSpeedX = 0.5F;
+    //public float translateSpeedZ = 0.5F;
+    public float translateScale = 0.1F;
+    public float translateZOffset = 1.5f;
+    //public AnimationCurve moveX;
+    //public AnimationCurve moveZ;
+    public int direction = 1;
 
     void Update()
     {
         transform.Rotate(new Vector3(0, 0, 1) * rotateSpeed * Time.deltaTime);
-        float angle = Mathf.Abs(transform.localRotation.eulerAngles.y );
-        Debug.Log(angle);
+        //float angle = Mathf.Abs(transform.localRotation.eulerAngles.z * translateScale);
+        /*
         transform.parent.Translate((Vector3.left * translateSpeed * moveX.Evaluate(angle)  
                                     + 
-                                    Vector3.forward * translateSpeed * moveY.Evaluate(angle)
-                                    ) * Time.deltaTime);
+                                    Vector3.forward * translateSpeed * moveX.Evaluate(angle+ translateZOffset)
+                                    ) * Time.deltaTime);*/
+
+        
+        Vector3 dir = (Vector3.left * translateSpeed
+                                    +
+                                    Vector3.forward * translateSpeed
+                                    ) * Time.deltaTime * direction;
+
+        transform.parent.Translate((Vector3.left * translateSpeed
+                                    +
+                                    Vector3.forward * translateSpeed
+                                    ) * Time.deltaTime * direction);
+        //Debug.Log(dir);
     }
 }
