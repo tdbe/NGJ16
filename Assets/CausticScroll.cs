9be3b7c97d22cedb6d7fb7e9993edba449891722
twodordan <twodordan@gmail.com>
2016-04-09 17:26:32 +0200

    Trying to fix meta file conflict on the fbx assets

---
+++ Assets/CausticScroll.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CausticScroll : MonoBehaviour {
+
+    public float rotateSpeed = 0.5F;
+
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 0, 1) * rotateSpeed);
+    }
+}
